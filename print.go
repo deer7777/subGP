3c78659fbc97717571b620a2f13926437abb4a62
congxia wu <1398173038@qq.com>
2024-04-25 11:19:29 +0800

    v2.1.0 + subsubGM

--- print.go
+++ print.go
@@ -1,9 +1,9 @@
 package subGP
 
 import "fmt"
-import "github.com/deer7777/subsubGP"
+import "github.com/deer7777/subsubGM"
 
 func Print() {
-	fmt.Println("subGP v1.0.0")
-	fmt.Println(subsubGP.Print())
+	fmt.Println("subGP v2.1.0")
+	fmt.Println(subsubGM.Print())
 }
